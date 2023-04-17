f80c0e3a9f5e079b6c84d04b7a75464ded6c445d
hanjun <hanjun@yiye.ai>
2023-04-17 13:03:40 +0800

    不走代理

--- internal/gin/gin.go
+++ internal/gin/gin.go
@@ -4,6 +4,12 @@ import "github.com/gin-gonic/gin"
 
 func StartServer() {
 	r := gin.Default()
-	ProxyOpenAI(r)
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "hello",
+		})
+	})
+	// ProxyOpenAI(r)
+	RequestOpenAi(r)
 	r.Run(":9000")
 }
--- internal/gin/proxy.go
+++ internal/gin/proxy.go
@@ -1,11 +1,17 @@
 package gin
 
 import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 
+	"github.com/gin-contrib/sse"
 	"github.com/gin-gonic/gin"
+	"github.com/sashabaranov/go-openai"
 )
 
 type CustomTransport struct {
@@ -30,7 +36,7 @@ func ProxyOpenAI(r *gin.Engine) {
 		RoundTripper: http.DefaultTransport,
 	}
 	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
-		Scheme: "http",
+		Scheme: "https",
 		Host:   "openai.hanjunty.top",
 	})
 	// 设置传输对象
@@ -40,4 +46,67 @@ func ProxyOpenAI(r *gin.Engine) {
 		// 将请求转发到代理服务器
 		proxy.ServeHTTP(c.Writer, c.Request)
 	})
+}
+
+// 走请求模式 不走代理
+type OpenAIBody struct {
+	Messages []openai.ChatCompletionMessage `json:"messages"`
+}
+
+func RequestOpenAi(r *gin.Engine) {
+
+	r.POST("/chat", func(ctx *gin.Context) {
+		// 获取body 参数
+		// ChatMessageRoleSystem    = "system"
+		// ChatMessageRoleUser      = "user"
+		// ChatMessageRoleAssistant = "assistant"
+		var body OpenAIBody
+		if err := ctx.BindJSON(&body); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		config := openai.DefaultConfig("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")
+		config.BaseURL = "https://openai.hanjunty.top/v1"
+		c := openai.NewClientWithConfig(config)
+		octx := context.Background()
+
+		req := openai.ChatCompletionRequest{
+			Model:     openai.GPT3Dot5Turbo,
+			MaxTokens: 20,
+			Messages:  body.Messages,
+			// Messages: []openai.ChatCompletionMessage{
+			// 	{
+			// 		Role:    openai.ChatMessageRoleUser,
+			// 		Content: "你好",
+			// 	},
+			// },
+			Stream: true,
+		}
+
+		stream, err := c.CreateChatCompletionStream(octx, req)
+		if err != nil {
+			fmt.Printf("ChatCompletionStream error: %v\n", err)
+			return
+		}
+		defer stream.Close()
+		fmt.Printf("Stream response: ")
+		for {
+			response, err := stream.Recv()
+			if errors.Is(err, io.EOF) {
+				fmt.Println("\nstream finished")
+				return
+			}
+			if err != nil {
+				fmt.Printf("\nstream error: %v\n", err)
+				return
+			}
+			sse.Encode(ctx.Writer, sse.Event{
+				Event: "message",
+				Data:  response.Choices[0].Delta.Content,
+			})
+			// 刷新数据，以通知请求端
+			ctx.Writer.Flush()
+		}
+	})
+
 }
